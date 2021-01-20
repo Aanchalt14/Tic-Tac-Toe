5963f1d612169d6064faac1bcf992fb34c39467f
Aanchalt14 <aanchalt71@gmail.com>
2021-01-20 12:15:26 +0530

    Added : Show Board so user can choose valid cell for move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,15 @@
 package com.workshop;
 
+import java.util.Scanner;
+
 public class Tictactoe {
 
     public static void main(String[] args) {
         char[] board=createBoard();
+        Scanner userInput=new Scanner(System.in);
+        char userLetter =chooseUserLetter(userInput);
+        char computeLetter =(userLetter == 'X')?'0':'x';
+        showBoard(board);
     }
     private static char[] createBoard(){
         char[] board=new char[10];
@@ -11,5 +17,22 @@ public class Tictactoe {
             board[i]=' ';
         }
         return board;
+    }
+    private static char chooseUserLetter(Scanner userInput) {
+        System.out.println("choose your letter: ");
+        return userInput.next().toUpperCase().charAt(0);
+    }
+    public static void showBoard(char[] Board)
+    {
+        System.out.println("");
+        for(int i =1 ; i<Board.length ;i++)
+        {
+            System.out.print("|_"+Board[i] + "_|");
+            if(i%3==0)
+            {
+                System.out.print(System.lineSeparator());
+                System.out.println("---------------");
+            }
+        }
     }
 }
